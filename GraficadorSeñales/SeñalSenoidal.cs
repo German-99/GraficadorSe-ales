e7cb67d9c42245c733f58d7de4d949c6e1720278
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-08-13 11:48:52 -0700

    Agregar archivos de proyecto.

---
+++ GraficadorSeñales/SeñalSenoidal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraficadorSeñales
+{
+    class SeñalSenoidal
+    {
+        //Auto prperties
+        //Atributos encapsulados de c#
+        //No tienen rutinas al establecer u obtener valor
+        public double Amplitud { get; set; }
+        public double Fase { get; set; }
+        public double Frecuencia { get; set; }
+
+        //Constrtuctor se ejecuta al instanciar la clase
+        //Sintaxis: public NombreClase(Parametros)
+
+        public SeñalSenoidal()
+        {
+            Amplitud = 1.0;
+            Fase = 0.0;
+            Frecuencia = 1.0;
+        }
+
+        public SeñalSenoidal(double amplitud, double fase, double frecuencia)
+        {
+            Amplitud = amplitud;
+            Fase = fase;
+            Frecuencia = frecuencia;
+        }
+
+        public double evaluar(double tiempo)
+        {
+            double resultado = 0.0;
+
+            //Evaluar la función
+            resultado = Amplitud * Math.Sin((2.0 * Math.PI * Frecuencia * tiempo) + Fase);
+            return resultado;
+        }
+
+    }
+}
