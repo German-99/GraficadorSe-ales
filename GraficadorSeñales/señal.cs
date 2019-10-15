276af5f1d4d9d6c5cf938eaef4443be13ced9ce6
Santiago Salcido <santiago.salcido@hotmail.com>
2019-10-15 11:33:57 -0700

    Fourier

--- GraficadorSeñales/señal.cs
+++ GraficadorSeñales/señal.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace GraficadorSeñales
 {
@@ -69,6 +70,59 @@ namespace GraficadorSeñales
                     resultado.AmplitudMaxima =
                         Math.Abs(nuevoValor);
                 }
+            }
+            return resultado;
+        }
+
+        public static Señal transdormadaFourier(Señal señal)
+        {
+            SeñalResultante resultado = new SeñalResultante();
+
+            resultado.TiempoInicial = señal.TiempoInicial;
+            resultado.TiempoFinal = señal.TiempoFinal;
+            resultado.FrecuenciaMuestreo = señal.FrecuenciaMuestreo;
+
+            for (int k = 0; k < señal.Muestras.Count; k++)
+            {
+                Complex muestra = 0; // 0 = 0i
+                for (int n = 0; n < señal.Muestras.Count; n++)
+                {
+                    muestra += señal.Muestras[n].Y * Complex.Exp((-2 * Math.PI * Complex.ImaginaryOne * k * n) / señal.Muestras.Count);
+                }
+                resultado.Muestras.Add(new Muestra(señal.Muestras[k].X, muestra.Magnitude));
+            }
+            return resultado;
+        }
+        public static Señal multiplicarSeñales(
+            Señal señal1, Señal señal2)
+        {
+            SeñalResultante resultado =
+                new SeñalResultante();
+            resultado.TiempoInicial =
+                señal1.TiempoInicial;
+            resultado.TiempoFinal =
+                señal1.TiempoFinal;
+            resultado.FrecuenciaMuestreo =
+                señal1.FrecuenciaMuestreo;
+
+            int indice = 0;
+            foreach (var muestra in señal1.Muestras)
+            {
+                double nuevoValor =
+                    muestra.Y *
+                    señal2.Muestras[indice].Y;
+
+                resultado.Muestras.Add(
+                    new Muestra(muestra.X, nuevoValor));
+
+                if (Math.Abs(nuevoValor) >
+                    resultado.AmplitudMaxima)
+                {
+                    resultado.AmplitudMaxima =
+                        Math.Abs(nuevoValor);
+                }
+
+                indice++;
             }
             return resultado;
         }
